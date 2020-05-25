7e00dfe1f998911a791624e40cb2bc555abb70d8
IsadoraMartinez <isadoramartinez123@gmail.com>
2020-05-25 19:39:55 -0400

    Add SearchBar.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,4 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App'
 
-// class App extends React.Component{
-
-// 	render(){
-// 		return <div>it's working</div>;
-// 	}
-// };
-
 ReactDOM.render(<App />, document.querySelector("#root"));
