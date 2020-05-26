bf71e280b244298c776805cad287ec131383dcc5
IsadoraMartinez <isadoramartinez123@gmail.com>
2020-05-25 20:59:51 -0400

    Invoke callback into children of App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => {
-	return (
-		<div className="ui container" style={{marginTop:'10px'}}><SearchBar/></div>
-		);
-};
+class App extends React.Component {
+	
+	onSearchSubmit(term){
+		console.log(term);
+	}
 
-export default App;
+	render(){
+		return (
+			<div className="ui container" style={{marginTop:'10px'}}>
+			<SearchBar onSubmit={this.onSearchSubmit}/>
+			</div>
+			);
+		}
+	}
+
+	export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,10 +8,16 @@ class SearchBar extends React.Component{
 
 	state = {term:''};
 
+	onFormSubmit = (event) => {
+		event.preventDefault();
+ 	
+ 		this.props.onSubmit(this.state.term);
+	};
+
 	render(){
 		return (
 			<div className="ui segment">
-			<form className="ui form">
+			<form className="ui form" onSubmit={this.onFormSubmit}>
 			<div className="field">
 			<label>Image Search</label>
 			<input type='text' value={this.state.term} onChange={(e) => this.setState({term:e.target.value})} />
@@ -21,5 +27,6 @@ class SearchBar extends React.Component{
 			);
 	}
 }
+
 
 export default SearchBar;
