fa66d751014a7429509a47522d744fc21c0f222a
IsadoraMartinez <isadoramartinez123@gmail.com>
2020-05-28 20:51:44 -0400

    Fetch data from Unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
+import config from './Config'
+
+const accessKey = config.access_key;
+const secretKey = config.secret_key;
 
 class App extends React.Component {
+
+	
 	
 	onSearchSubmit(term){
-		console.log(term);
+		axios.get('https://api.unsplash.com/search/photos',{
+			params:{ query: term },
+			headers:{
+				Authorization: 'Client-ID ' + accessKey
+			}
+		});
 	}
 
 	render(){
@@ -14,7 +25,7 @@ class App extends React.Component {
 			<SearchBar onSubmit={this.onSearchSubmit}/>
 			</div>
 			);
-		}
 	}
+}
 
-	export default App;
+export default App;
